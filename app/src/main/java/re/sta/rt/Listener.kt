0a8ea6a87e8587d8edaf641711a2eaad93e87fb4
JUNGMIN AN <71064490+CHRIS-AN@users.noreply.github.com>
2021-03-29 06:03:15 +0900

    Android_20210329
    setOnClick

--- app/src/main/java/re/sta/rt/Listener.kt
+++ app/src/main/java/re/sta/rt/Listener.kt
@@ -1,7 +1,11 @@
 package re.sta.rt
 
+import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.ImageView
 import android.widget.TextView
 
 class Listener : AppCompatActivity() {
@@ -9,10 +13,47 @@ class Listener : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_listener)
 
+        val textView : TextView = findViewById(R.id.helloEveryOne)
+        val image : ImageView = findViewById(R.id.image)
+        var number = 10
+        // 익명함수 !!!!!!!!
 
-        // java 방법) view 를 activity 로 가져오는 방법.
-        //val textView : TextView = findViewById(R.id.hello)
 
-        // kotlin 방법)  xml 를 import 해서 가져온다.
+        //java 방법) view 를 activity 로 가져오는 방법.
+        // 1 : 람다 방식
+            textView.setOnClickListener{
+                Log.d("click", "Click !!")
+            }
+
+        // 2 : 익명 함수 방식
+        textView.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                Log.d("click", "Click !!")
+            }
+        })
+
+        // 3 : 이름이 필요한 경우
+        val click = object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                Log.d("click", "Click !!")
+                textView.setText("안녕하세요")
+                image.setImageResource(R.drawable.water)
+                number += 10
+                Log.d("number", ""+number)
+            }
+        }
+        textView.setOnClickListener(click)
+
+
+
+
+
+        // 뷰를 조작하는 함수들
+
+
+
+
+
+
     }
 }
