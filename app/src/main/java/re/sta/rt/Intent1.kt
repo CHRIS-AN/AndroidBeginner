5ef89c7b264a1a5eca4ecc8f6b02401596200564
JUNGMIN AN <71064490+CHRIS-AN@users.noreply.github.com>
2021-04-05 21:42:35 +0900

    Android (intent, Task)

--- app/src/main/java/re/sta/rt/Intent1.kt
+++ app/src/main/java/re/sta/rt/Intent1.kt
@@ -1,8 +1,10 @@
 package re.sta.rt
 
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 
 class Intent1 : AppCompatActivity() {
@@ -14,26 +16,53 @@ class Intent1 : AppCompatActivity() {
 
 
         change_activity.setOnClickListener {
-            val intent = Intent(this@Intent1, Intent2::class.java)
+//            val intent = Intent(this@Intent1, Intent2::class.java)
+//            // Key : Value 방식  - > Key 와 Value 를 쌍으로 만들어 저장
+//            // intent 에 값을 담아 넘긴다.
+//            intent.putExtra("number1", 1)
+//            intent.putExtra("number2", 2)
+//            startActivity(intent)
 
-            // Key : Value 방식  - > Key 와 Value 를 쌍으로 만들어 저장
-            // intent 에 값을 담아 넘긴다.
-            intent.putExtra("number", 1)
-            intent.putExtra("number2", 2)
+
+//            val intent2 = Intent(this@Intent1, Intent2::class.java)
+//            // apply 란? kotlin 에서 굉장히 유용하다.
+//            intent2.apply {
+//                this.putExtra("number1", 1)
+//                this.putExtra("number2", 2)
+//            }
+            //startActivity(intent2) 전달만하는 애
+
+            // 전달하고 결과도 받아볼 애애
+//           startActivityForResult(intent2, 200) // requestcode의 이름을 지정해준다.
+            // 왜 requestCode 를 작성하냐면, 결과를 받아볼 때, 내가 받은 결과가 맞는지 비교를 해주기 위해서 인수 이름을 지정해준다.
+
+
+
+
+            // * 암시적 인텐트
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse("http://m.naver.com"))
             startActivity(intent)
 
 
-            val intent2 = Intent(this@Intent1, Intent::class.java)
-            // apply 란? kotlin 에서 굉장히 유용하다.
-            intent2.apply {
-                this.putExtra("number1", 1)
-                this.putExtra("number2", 2)
-            }
-            startActivity(intent2)
 
         }
+    }
+    // Intent의 결과 값을 받으려면, 이 함수를 override를 해야한다.
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
 
-
-
+        if(requestCode == 200) {
+            Log.d("number", ""+requestCode)
+            Log.d("number", ""+resultCode)
+            // 200 이라는 인수이름이 맞다면? 데이터를 꺼내본다.
+            val result = data?.getIntExtra("result", 0)
+            Log.d("number", ""+result)
+        }
+        super.onActivityResult(requestCode, resultCode, data)
     }
 }
+
+
+
+
+
+
--- app/src/main/java/re/sta/rt/Intent2.kt
+++ app/src/main/java/re/sta/rt/Intent2.kt
@@ -1,8 +1,11 @@
 package re.sta.rt
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
 
 class Intent2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,12 +13,36 @@ class Intent2 : AppCompatActivity() {
         setContentView(R.layout.activity_intent2)
 
 
-        val number1 = intent.getIntExtra("number1", 0)
-        val number2 = intent.getIntExtra("number", 0)
+        val result : Button = findViewById(R.id.result)
 
-        Log.d("number", ""+number1)
-        Log.d("number", ""+number2)
+        result.setOnClickListener {
+            val number1 = intent.getIntExtra("number1", 0)
+            val number2 = intent.getIntExtra("number2", 0)
 
+            Log.d("number", ""+number1)
+            Log.d("number", ""+number2)
+
+            val result = number1 + number2
+
+
+            val resultIntent = Intent() // 인텐트를 생성 후,
+            resultIntent.putExtra("result", result) // 생성한 인텐트에다가 결과값을 넣어준다.
+
+
+            // 결과를 보내는 쪽에서 이 함수를 통해서 보낸다.
+            setResult(Activity.RESULT_OK, resultIntent) // 결과를 설정해주는 것이다. -1
+            finish() // -> Activity 종류 중 하나.
+
+                // Stack 이라는 개념 ( 위로 쌓는다 )
+                // Intent2 --> 종료 시킴 finish() 로 인하여.
+                // Intent1
+        }
 
     }
 }
+
+
+
+
+
+
